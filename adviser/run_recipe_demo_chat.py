cbdc38c598a976cc91c2913b3c6a0d1c7057df03
Tom Zeller <st101288@stud.uni-stuttgart.de>
2022-08-14 11:55:55 +0200

    remove sys.path log

--- adviser/run_recipe_demo_chat.py
+++ adviser/run_recipe_demo_chat.py
@@ -19,9 +19,7 @@ import os
 def get_root_dir():
     return os.path.dirname(os.path.dirname(os.path.join("..", (os.path.abspath(__file__)))))
 
-
 sys.path.append(get_root_dir())
-print(sys.path)
 
 
 # import domain class and logger
